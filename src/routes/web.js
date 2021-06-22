62c13b9bf5d0cdf5d200729eff4cd02ab0c3c776
marurabanal <marurabanal@gmail.com>
2021-06-22 11:24:48 -0300

    Trabajando en detalle Productos

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "../views/index.html")))
-app.get("/productDetail/", (req, res) => res.sendFile(path.resolve(__dirname, "../views/productDetail.html")))
+app.get("/productDetails/", (req, res) => res.sendFile(path.resolve(__dirname, "../views/productDetails.html")))
 app.get("/productCart/", (req, res) => res.sendFile(path.resolve(__dirname, "../views/productCart.html")))
 app.get("/register/", (req, res) => res.sendFile(path.resolve(__dirname, "../views/register.html")))
 app.get("/login/", (req, res) => res.sendFile(path.resolve(__dirname, "../views/login.html")))
